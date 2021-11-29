35a42f99a747da7450bf5c0fd70b82cf4ef76c74
Dennis Eller <dennis.eller@fh-bielefeld.de>
2021-11-29 20:23:31 +0100

    Decluttered laser

--- Assets/Weapons/Laser.cs
+++ Assets/Weapons/Laser.cs
@@ -1,41 +1,24 @@
-using System.Collections;
 using Assets.Weapons;
-using Unity.VisualScripting;
 using UnityEngine;
 
 
 public class Laser : BaseWeapon
 {
-    [SerializeField]
-    private Transform firepoint;
-    public float range = 500;
-    public WeaponType Type { get; private set; }
-
-    public bool shooted = false;
-    private LineRenderer lineRenderer;
-
-    private ParticleSystem overheatParticleSystem;
-
-    public float condition = 100f;
-    public float maxCondition = 100f;
-    public bool overHeated = false;
-
-    public float heatRate = 25f;
-    public float cooldownRate = 10f;
-    public float cooldownTime = 5;
-
-    private float deltaTime;
     
+    public float range = 500;
+    public bool shooted = false;
+    public bool overHeated = false;
+    private LineRenderer lineRenderer;
+    public float deltaTime;
+
     void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        overheatParticleSystem = GetComponentInChildren<ParticleSystem>();
-        overheatParticleSystem.Stop();
-        Type = WeaponType.LASER;
-        deltaTime = useCooldown+1;
+        deltaTime = atackSpeed+1;
+        
     }
     
-    void UpdateRoute()
+    public void UpdateRoute()
     {
         if (!shooted || overHeated){
             lineRenderer.enabled = false;
@@ -44,27 +27,24 @@ public class Laser : BaseWeapon
 
         shooted = false;
         lineRenderer.enabled = true; 
-        lineRenderer.SetPosition(0, firepoint.position);
-        if (Physics.Raycast(transform.parent.parent.position, transform.parent.parent.forward, out var hit))
+        lineRenderer.SetPosition(0, Firepoint.position);
+        if (Physics.Raycast(Camera.position, Camera.forward, out var hit))
         {
             lineRenderer.SetPosition(1, hit.point);
-            
             var enemy = hit.collider.gameObject.GetComponent<CombatParticipant>();
             if (!enemy) { return; }
             var parent = Owner.GetComponent<CombatParticipant>();
             
-            if (!(deltaTime > useCooldown)) return;
+            if (!(deltaTime > atackSpeed)) return;
             deltaTime = 0f;
             enemy.TakeDamage(parent);
         }
         else
         {
-            lineRenderer.SetPosition(1,transform.parent.parent.forward * range);
+            lineRenderer.SetPosition(1,Camera.forward * range);
         }
-        
-        
-        
     }
+    
     public override void Use()
     {
         shooted = true;
@@ -73,53 +53,6 @@ public class Laser : BaseWeapon
     public void LateUpdate()
     {
         deltaTime += Time.deltaTime;
-        handleHeatingAndCooling();
         UpdateRoute();
-        DrawParticleSystem();
-      
-    }
-    
-    private void handleHeatingAndCooling()
-    {
-        if (shooted && !overHeated)
-        {
-            condition -= Time.deltaTime*heatRate;
-            if (condition < 0)
-            {
-                condition = 0;
-                overHeated = true;
-                StartCoroutine(StartWeaponCooldown());
-            }
-        }
-        else if(!shooted && condition<maxCondition && !overHeated)
-        {
-            condition += Time.deltaTime*cooldownRate;
-            if (condition > maxCondition)
-            {
-                condition = maxCondition;
-            }
-        }
-       
-               
-    }
-    
-
-    private void DrawParticleSystem()
-    {
-        if (overHeated && overheatParticleSystem.isStopped)
-        {
-            overheatParticleSystem.Play();
-        }
-        else if(!overHeated &&  overheatParticleSystem.isPlaying)
-        {
-            overheatParticleSystem.Stop();
-        }
-    }
-
-    IEnumerator StartWeaponCooldown()
-    {
-        shooted = false;
-        yield return new WaitForSeconds(cooldownTime); 
-        overHeated = false;
     }
 }
