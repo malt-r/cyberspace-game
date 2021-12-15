0a557cb219fd30dc31e98cc393874178d0e79a80
deller <dennis.eller@fh-bielefeld.de>
2021-12-15 23:19:45 +0100

    Add Startsound to background in menue

--- Assets/MainMenue.cs
+++ Assets/MainMenue.cs
@@ -1,10 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenue : MonoBehaviour
 {
    [SerializeField] private string playScene;
-   
+
+   private void Start()
+   {
+      SoundManager.Instance.SetBackgroundMusic("Menue");
+   }
+
    public void PlayGame()
    {
       SoundManager.Instance.SetBackgroundMusic("Game");
