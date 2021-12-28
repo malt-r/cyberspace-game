744d8bc2e2eccf7f83b348f51c2e84314a7e29e6
deller <dennis.eller@fh-bielefeld.de>
2021-12-28 18:01:12 +0100

    Add Mouse sensitivity settings

--- Assets/Player/FirstPersonController.cs
+++ Assets/Player/FirstPersonController.cs
@@ -74,7 +74,9 @@ namespace StarterAssets
 		private float xRotationVelocity;
 		private float _verticalVelocity;
 		private float _terminalVelocity = 53.0f;
-
+		
+		
+		
 		// minigame
 		private PlayerInput _playerInput;
 
@@ -106,8 +108,21 @@ namespace StarterAssets
 			// reset our timeouts on start
 			_jumpTimeoutDelta = JumpTimeout;
 			_fallTimeoutDelta = FallTimeout;
+			
+			if (PlayerPrefs.HasKey("Mouse/Sensitivity"))
+			{
+				RotationSpeed = PlayerPrefs.GetFloat("Mouse/Sensitivity");
+			}
+			
+			EventManager.StartListening("Mouse/SensitivityChanged",handleSensitivityChanged);
 		}
 
+		void handleSensitivityChanged(object data)
+		{
+			var newSensitivity = data is float f ? f : 0;
+			RotationSpeed = newSensitivity;
+		}
+		
 		private void Update()
 		{
 			if (_prevCanSee != canSee)
@@ -157,7 +172,8 @@ namespace StarterAssets
 
 				// clamp our pitch rotation
 				yRotationVelocity = ClampAngle(yRotationVelocity, BottomClamp, TopClamp);
-				
+
+
 				// rotate the player left and right
 				transform.Rotate(Vector3.up * xRotationVelocity);
 				MainCamera.transform.localRotation = Quaternion.Euler(yRotationVelocity, 0.0f, 0.0f);
