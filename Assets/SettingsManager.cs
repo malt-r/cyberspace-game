f5d58e480f3b6b3028875182554cb40e9b4e0955
deller <dennis.eller@fh-bielefeld.de>
2021-12-28 18:01:49 +0100

    Add Settingsmanager to set PlayerPrefs

---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsManager : MonoBehaviour
+{
+  private static SettingsManager _instance;
+  public static SettingsManager Instance => _instance;
+
+  public float MouseSensitivity = 5f;
+
+  void Start()
+  {
+
+  }
+
+  public void SetMouseSensitivity(float sensitivity)
+  {
+    PlayerPrefs.SetFloat("Mouse/Sensitivity",sensitivity);
+    EventManager.TriggerEvent("MouseSensitivityChanged",sensitivity);
+  }
+
+
+  
+}
