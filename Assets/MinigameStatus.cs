643bbeaa60f338d63a352f812fc1f6ad56a540ea
Malt_R <malte.r@mail.de>
2022-01-01 17:51:11 +0100

    Turn off block animation, if minigame finished

--- Assets/MinigameStatus.cs
+++ Assets/MinigameStatus.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class MinigameStatus : MonoBehaviour
 {
-
     public bool isDone = false;
     private StoryTrigger _storyTrigger;
+
+    private Animator _animator;
     
     // Start is called before the first frame update
     void Start()
     {
         _storyTrigger = GetComponent<StoryTrigger>();
+        _animator = GetComponent<Animator>();
+        _animator.SetTrigger("Start");
     }
 
     // Update is called once per frame
@@ -28,6 +31,11 @@ public class MinigameStatus : MonoBehaviour
         if (_storyTrigger != null)
         {
             _storyTrigger.Activate();
+        }
+
+        if (_animator != null)
+        {
+            _animator.SetTrigger("End");
         }
     }
 }
