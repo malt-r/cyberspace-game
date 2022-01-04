155b81ce6acdd48544113c7368afa5080e148a99
Dennis Eller <dennis.eller@fh-bielefeld.de>
2022-01-04 14:52:43 +0100

    add playergun prefab

---
+++ Assets/PickupScanner.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScanner : MonoBehaviour
+{
+    [SerializeField] private GameObject scanner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scanner = GameObject.Find("/PlayerCapsule").transform.GetChild(1).GetChild(0).GetChild(0).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {   
+        scanner.SetActive(true);
+        SoundManager.PlaySound(Sound.ItemPickup);
+        Destroy(gameObject);
+    }
+}
