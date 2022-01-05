82ee37de9fffd11bf0e83c7fd13ef6b5a621a08a
Malt_R <malte.r@mail.de>
2022-01-05 22:07:06 +0100

    Modify text2spech tool; small modifications; add trigger in endroom

--- Assets/Story/Triggers/EnterColliderTrigger.cs
+++ Assets/Story/Triggers/EnterColliderTrigger.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EnterColliderTrigger : VoiceTrigger
 {
     private Collider _collider;
+
     // Start is called before the first frame update
     void Start()
     {
