3698fb4ed379a2a810c39b29345e684effd35396
timl <tim.luecking1@fh-bielefeld.de>
2022-01-05 18:12:05 +0100

    Add edit testscene to gen level2

--- Assets/PickupScanner.cs
+++ Assets/PickupScanner.cs
@@ -3,19 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickupScanner : MonoBehaviour
+public class PickupScanner : BaseItem
 {
     [SerializeField] private GameObject scanner;
     // Start is called before the first frame update
     void Start()
     {
-        scanner = GameObject.Find("/PlayerCapsule").transform.GetChild(1).GetChild(0).GetChild(0).gameObject;
+        
+    }
+
+    public override void Visit(Transform player)
+    {
+        //throw new NotImplementedException();
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        scanner = GameObject.Find("/PlayerCapsule(Clone)").transform.GetChild(1).GetChild(0).GetChild(0).gameObject;
         if (!other.tag.Equals("Player")) { return; }
         scanner.SetActive(true);
+        base.OnPickup();
         SoundManager.PlaySound(Sound.ItemPickup);
         Destroy(gameObject);
     }
