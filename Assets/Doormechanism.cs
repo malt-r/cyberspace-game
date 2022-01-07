972a65caaf786b55d68c25b7884dff3bf1a154d7
Malt_R <malte.r@mail.de>
2022-01-07 23:08:09 +0100

    deactivate door sound on start

--- Assets/Doormechanism.cs
+++ Assets/Doormechanism.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
 
 public class Doormechanism : MonoBehaviour
 {
@@ -14,13 +16,15 @@ public class Doormechanism : MonoBehaviour
     public GameObject[] enemies;
 
     private bool done = false;
+    private bool open = true;
+    private bool audioActive = true;
         
     // Start is called before the first frame update
     protected void Start()
     {
         if (!StartWithDoorsOpen) // door will start in open state (set by animator)
         {
-            closeDoors();
+            closeDoors(true);
         }
     }
 
@@ -37,8 +41,18 @@ public class Doormechanism : MonoBehaviour
             return;
         }
 
+        if (areAllEnemiesDead())
+        {
+            return;
+        }
+
         other.GetComponent<ActorStats>().OnHealthReachedZero += openDoors;
         closeDoors();
+    }
+
+    public void openDoors()
+    {
+        openDoors(false);
     }
 
     public void OnTriggerExit(Collider other)
@@ -47,6 +61,12 @@ public class Doormechanism : MonoBehaviour
         {
             return;
         }
+
+        if (open)
+        {
+            return;
+        }
+        
         openDoors();
     }
 
@@ -60,21 +80,51 @@ public class Doormechanism : MonoBehaviour
         }
     }
 
-    public void openDoors()
+    private void SetAudioState(bool deactivateAudio)
     {
+        if (audioActive && deactivateAudio)
+        {
+            foreach (var door in Doors)
+            {
+                door.GetComponent<AudioSource>().enabled = false;
+            }
+
+            audioActive = false;
+        }
+        else if (!audioActive && !deactivateAudio)
+        {
+            foreach (var door in Doors)
+            {
+                door.GetComponent<AudioSource>().enabled = true;
+            }
+
+            audioActive = true;
+        }
+    }
+
+    public void openDoors(bool deactivateAudio = false)
+    {
+        SetAudioState(deactivateAudio);
+        
         foreach (var door in Doors)
         {
             door.GetComponent<Animator>().SetTrigger("Open");
         }
+
+        open = true;
     }
 
-    public void closeDoors()
+    public void closeDoors(bool deactivateAudio = false)
     {
+        SetAudioState(deactivateAudio);
+        
         foreach (var door in Doors)
         {
             door.setDoorActive(true);
             door.GetComponent<Animator>().SetTrigger("Close");
         }
+
+        open = false;
     }
 
     private bool areAllEnemiesDead()
